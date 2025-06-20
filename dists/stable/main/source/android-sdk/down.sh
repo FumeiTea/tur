87e822ac9d952198dafd7a69547fc56fe161aca0
fumei <wwb.cici@gmail.com>
2025-06-20 11:26:41 +0800

    1

---
+++ dists/stable/main/source/android-sdk/down.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+i=0
+F=$PREFIX/tmp/android-sdk.tar.xz
+DIR=$PREFIX/opt/android-sdk
+MD5="ee664b3a73737a7420856c4c8641665a"
+
+[[ $1 == -rm ]] && rm $F
+
+[ -f $F ] && mapfile -t -d ' ' md5 < <(md5sum $PREFIX/tmp/android-sdk.tar.xz)
+[[ $MD5 != ${md5[0]} ]] && while ((i<3)) ;do
+  if echo curl -L -C - -o $F https://github.com/AndroidIDEOfficial/androidide-tools/releases/download/sdk/android-sdk.tar.xz ;then
+    echo down flished
+    break
+  fi
+  ((i++))
+  (( i>3)) && echo down err
+done
+
+[ ! -d $DIR ] && [ ! -e $DIR ] && mkdir $DIR
+[ ! -d $DIR ] && echo ! not is dir && exit -1
+
+echo tar
+i=0
+: "${COLUMNS:=$(stty size 2>/dev/null | awk '{print $2}')}"
+COLUMNS=$((COLUMNS-10))
+while read -r l ;do
+  ((i++))
+  printf "\033[2K\r%-6s %.${COLUMNS}s" $(( i / 66 ))%  $l
+done < <(tar -xvf $F -C $DIR)
+printf "\033[2K\r"
+[[ -d $DIR/android-sdk/ ]] && mv $DIR/android-sdk/* $DIR &>/dev/null
+
+# vim: set ft=sh ts=2 sw=2 sts=2 et :
